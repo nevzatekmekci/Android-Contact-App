ada352ff2289c5b13fef8ad6dafb82399d593ec3
“[Nevzat <[nvztekmekci@gmail.com]>
2015-12-16 23:15:19 +0200

    First Commit

--- SemesterProject/app/src/main/java/com/example/nevzat/semesterproject/MainActivity.java
+++ SemesterProject/app/src/main/java/com/example/nevzat/semesterproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView deneme = (TextView) findViewById(R.id.text);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
