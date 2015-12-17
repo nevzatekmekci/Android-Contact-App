fbc4c3717833d2d272ed3691893ff198307a0d6a
Nevzat Ekmekci <nvztekmekci@gmail.com>
2015-12-18 00:43:17 +0200

    Some modification

---
+++ SemesterProject/app/src/main/java/com/example/nevzat/semesterproject/models/ActivityStatistic.java
@@ -0,0 +1,7 @@
+package com.example.nevzat.semesterproject.models;
+
+/**
+ * Created by nevzat on 18/12/15.
+ */
+public class ActivityStatistic {
+}
--- SemesterProject/app/src/main/java/com/example/nevzat/semesterproject/models/Person.java
+++ SemesterProject/app/src/main/java/com/example/nevzat/semesterproject/models/Person.java
@@ -1,4 +1,4 @@
-package com.example.nevzat.semesterproject.classes;
+package com.example.nevzat.semesterproject.models;
 
 /**
  * Created by nevzat on 18/12/15.
